20104614f4fb338a2baf122cc101315801e8fbeb
EloyMartinez <eloyalbmartinez@gmail.com>
2022-03-29 18:28:09 -0400

    create microservice

--- microServiceRecherche/Controllers/rechercheController.cs
+++ microServiceRecherche/Controllers/rechercheController.cs
@@ -7,7 +7,7 @@ using microServiceRecherche.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-
+using Newtonsoft.Json;
 
 namespace microServiceRecherche.Controllers
 {
@@ -26,12 +26,50 @@ namespace microServiceRecherche.Controllers
             return db.Electriques.ToList();
         }
 
-        // GET: api/recherche/5
-        [HttpGet("{name}", Name = "Get")]
-        public IQueryable<Electrique> Get(string name)
+        // GET: api/recherche/glo
+        [HttpGet("advanced/{name}", Name = "advanced")]
+        public String advanced(string name)
         {
-            Debug.WriteLine("Send to debug output.");
-            return db.Electriques.Where( elec => elec.NomOutil.Contains(name));
+            IQueryable<Object> electrique = db.Electriques.Where(elec => elec.NomOutil.Contains(name));
+            IQueryable<Object> hydraulique = db.Hydrauliques.Where(elec => elec.NomOutil.Contains(name));
+            // TODO IQueryable<Hydraulique> manuel = db.Manuels.Where(elec => elec.NomOutil.Contains(name));
+            var jsonElectrique = JsonConvert.SerializeObject(electrique.ToArray());
+            var jsonHydraulique = JsonConvert.SerializeObject(hydraulique.ToArray());
+
+            return String.Concat(jsonElectrique,jsonHydraulique);
+            }
+
+        // GET: api/recherche/glo
+        [HttpGet("advanced", Name = "advancedDefault")]
+        public String advancedDefault()
+        {
+            List<Electrique> electrique = db.Electriques.ToList(); 
+            List<Hydraulique> hydraulique = db.Hydrauliques.ToList(); 
+            // TODO IQueryable<Hydraulique> manuel = db.Manuels.Where(elec => elec.NomOutil.Contains(name));
+            var jsonElectrique = JsonConvert.SerializeObject(electrique.ToArray());
+            var jsonHydraulique = JsonConvert.SerializeObject(hydraulique.ToArray());
+
+            return String.Concat(jsonElectrique, jsonHydraulique);
+        }
+
+        // GET: api/recherche/glo
+        [HttpGet("electrique/{name}", Name = "electrique")]
+        public String electrique(string name)
+        {
+            IQueryable<Object> electrique = db.Electriques.Where(elec => elec.NomOutil.Contains(name));
+            var jsonElectrique = JsonConvert.SerializeObject(electrique.ToArray());
+
+            return jsonElectrique;
+        }
+
+        // GET: api/recherche/glo
+        [HttpGet("electrique", Name = "electriqueDefault")]
+        public String electriqueDefault()
+        {
+            List<Electrique> electrique = db.Electriques.ToList();
+            var jsonElectrique = JsonConvert.SerializeObject(electrique.ToArray());
+
+            return jsonElectrique;
         }
 
         // POST: api/recherche
